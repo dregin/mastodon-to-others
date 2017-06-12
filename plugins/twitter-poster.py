4b3bb92bc30ee2c8be7559052a45547c70262434
bernard <dregin@gmail.com>
2017-06-13 00:20:08 +0100

    handle multiple hashtags and some error handling

--- plugins/twitter-poster.py
+++ plugins/twitter-poster.py
@@ -17,12 +17,21 @@ def post(post):
         print("Please create config: {}".format(config_location))
         print("Use helper script @ https://github.com/bear/python-twitter/blob/master/get_access_token.py")
 
-    share_token = config['share_token']
-    if share_token in post['summary']:
+    share_token = config['share_token'].strip('#')
+    tag_present = False
+    for tag in post['tags']:
+        if tag.term == share_token:
+            tag_present = True
+
+    if tag_present:
         api = twitter.Api(consumer_key=config['consumer_key'],
                 consumer_secret=config['consumer_secret'],
                 access_token_key=config['access_token_key'],
                 access_token_secret=config['access_token_secret'])
-        api.PostUpdate(post['link'])
+        try:
+            api.PostUpdate(post['link'])
+            print('Posted to twitter')
+        except twitter.TwitterError as e:
+            print(e.message[0]['message'])
     else:
         print("Post doesn't contain the share token - {}".format(share_token))
